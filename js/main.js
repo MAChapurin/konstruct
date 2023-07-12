e606db5aaf208155174fe69e3962197b3212fb09
Michael <machapurin@icloud.com>
2023-07-12 21:16:16 +0300

    added animation to header

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
 const copyDate = document.getElementById('copy-date');
 copyDate.textContent = new Date().getFullYear();
 
-const LINKS = document.querySelectorAll('.nav__link, .header__link, .footer__link');
+const LINKS = document.querySelectorAll(
+  '.nav__link, .header__link, .footer__link'
+);
 
-LINKS.forEach((link)=> {
+LINKS.forEach((link) => {
   link.addEventListener('click', function (e) {
     e.preventDefault();
     const id = link.getAttribute('href');
     document.querySelector(id).scrollIntoView({
-        behavior: 'smooth',
-        block: 'start'
+      behavior: 'smooth',
+      block: 'start',
     });
+  });
 });
-})
